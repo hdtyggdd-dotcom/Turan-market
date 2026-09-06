e6cf4d0fd7248e533c3b5e619f5102227cb95f24
Fazllidin Tursunqulov <hdtyggdd@gmail.com>
2026-09-07 00:18:12 +0500

    feat: add postcss configuration

---
+++ artifacts/web/postcss.config.js
@@ -0,0 +1,10 @@
+import postcss from 'postcss';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+module.exports = {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
+};
